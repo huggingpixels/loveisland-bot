17a1f0299912937bef36dfe7c78d298f2a45ca79
Eszter Matyas <eszter.matyas@quest.com>
2021-07-08 17:01:55 +0200

    Hello world

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"log"
+)
+
+func main() {
+	// Fiber instance
+	app := fiber.New()
+
+	// Routes
+	app.Get("/", hello)
+
+	// Start server
+	log.Fatal(app.Listen(":3000"))
+}
+
+// Handler
+func hello(c *fiber.Ctx) error {
+	return c.SendString("Hello, World 👋!")
+}
